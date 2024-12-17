6ae266e1d37efda596c194d5729f4bd0843d3e8a
Prajjwal Rawat <prajjwalrawat48@gmail.com>
2024-12-17 15:11:45 +0530

    updated

--- app/(root)/transformations/[id]/page.tsx
+++ app/(root)/transformations/[id]/page.tsx
@@ -9,8 +9,9 @@ import { getImageById } from "@/lib/actions/image.actions";
 import { getImageSize } from "@/lib/utils";
 import { DeleteConfirmation } from "@/components/shared/DeleteConfirmation";
 
-const ImageDetails = async ({ params: {id} }: SearchParamProps) => {
+const ImageDetails = async ({ params }: { params: { id: string } }) => {
 
+  const { id } = params;
   const { userId } = await auth();
 
   const image = await getImageById(id);
